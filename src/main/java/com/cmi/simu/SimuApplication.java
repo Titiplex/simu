b741b6867a295029eaf99222c24e7cd257f34e06
Titiplex <107863779+Titiplex@users.noreply.github.com>
2025-03-06 19:31:31 +0100

    déso j'avais pas mis les bons noms de projet

---
+++ src/main/java/com/cmi/simu/SimuApplication.java
@@ -0,0 +1,13 @@
+package com.cmi.simu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmi/simu/SimuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmi.simu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
