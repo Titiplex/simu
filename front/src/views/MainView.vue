1221a26e1c30588682469858ff6ec8dcb6d8d40a
Qwantike <paul.moinereau@hotmail.fr>
2025-03-07 08:47:05 +0100

    try api

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="main-view">
-        <!-- <Hospitals /> -->
+        <Hospitals />
         <MoneyBar :money="money" />
         <Map :money="money" @add-building="addBuilding" @open="openModal" />
-        <button @click="handleAddBuilding" class="add-building-button">Ajouter un hôpital: {{   Math.floor(Math.pow(buildings.length, 2)*100) }}</button>
+        <button @click="handleAddBuilding" class="add-building-button">Ajouter un hôpital: {{
+            Math.floor(Math.pow(buildings.length, 2) *100) }}</button>
         <!-- <Building v-for="(building, index) in buildings" :key="index" :building="building" @open="openModal" /> -->
         <Modal v-if="isModalOpen" :building="selectedBuilding" @close="isModalOpen = false" />
     </div>
@@ -57,7 +58,7 @@ export default {
         },
 
         addBuilding(building) {
-            const cost = Math.floor(Math.pow(this.buildings.length, 2)*100);
+            const cost = Math.floor(Math.pow(this.buildings.length, 2) * 100);
             if (this.money >= cost) {
                 const plot = document.getElementById(`city-${building.row}-${building.col}`);
                 if (plot) {
@@ -84,8 +85,8 @@ export default {
 
         //CRÉER LES BUILDINDS AVEC LES SERVICES ASSOCIÉS
         handleAddBuilding() {
-            const cost = Math.floor(Math.pow(this.buildings.length, 2)*100);
-            if(this.money < cost) {
+            const cost = Math.floor(Math.pow(this.buildings.length, 2) * 100);
+            if (this.money < cost) {
                 alert("Pas assez d'argent !");
                 return;
             }
